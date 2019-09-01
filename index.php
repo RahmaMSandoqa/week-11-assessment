70d2105a4393e3450fb2b9655ebd516b57576c03
sandoqarahma <rahma.san.m@gmail.com>
2019-09-01 18:03:29 +0300

    rahma's push

--- index.php
+++ index.php
@@ -1,9 +1,22 @@
 <?php
 
 /* Question 1 : Write a PHP script to display names and the salaries amounts, within a table. ou should use `echo ` to generate your table. */
-//////////////////////// Your Code Here /////////////////////
+<table>
+<tr>
+<td>
+    $rahma ='700'
+    $sandoqa = "3"
 
+    echo{
+        $name($rahma);
+        $name($sandoqa);
 
+    }
+
+</td>
+</tr>
+</table>
+?>
 
 
 
@@ -17,6 +30,25 @@ $countries = array( "Italy"=>"Rome",
                "Finland"=>"Helsinki",
                "France" => "Paris",
                "Slovakia"=>"Bratislava" ) ;
+
+               <?php
+
+$countries = array( "Italy"=>"Rome",
+              "Luxembourg"=>"Luxembourg",
+               "Belgium"=> "Brussels",
+               "Denmark"=>"Copenhagen",
+               "Finland"=>"Helsinki",
+               "France" => "Paris",
+               "Slovakia"=>"Bratislava" ) ;
+
+
+               foreach ($countries)
+        
+
+
+               ?>
+
+
 
 /*Question 3 : Write a script to build the following stars pattern, using a nested for loop.
 
@@ -33,6 +65,13 @@ $countries = array( "Italy"=>"Rome",
 
 */
 
+<?php
+        $stars="*";
+           for ($i=0; $i<5; $i++){
+               echo "$stars";
+        $star = $stars . "*";
+    }
+?>
 /*Question 4 : Write a PHP script to calculate the difference between two dates.
 Input : 1981-11-04, 2013-09-04
 Output : 31 years, 10 months, 11 days
